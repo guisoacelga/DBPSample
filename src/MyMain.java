f2783a8480d90b3b83f90265fec6f6f18211b2dd
guiso.acelga <guiso.acelga@gmail.com>
2021-08-25 14:01:09 +0200

    Initial commit

---
+++ src/MyMain.java
@@ -0,0 +1,27 @@
+import java.sql.*;
+public class MyMain {
+    public static void main(String[] args){
+
+        System.out.println("Hello Campus02");
+        createNewDatabase("t1.db");
+
+    }
+
+    public static void createNewDatabase(String fileName) {
+
+        String url = "jdbc:sqlite:C:/sqlite/db/" + fileName;
+
+        try (Connection conn = DriverManager.getConnection(url)) {
+            if (conn != null) {
+                DatabaseMetaData meta = conn.getMetaData();
+                System.out.println("The driver name is " + meta.getDriverName());
+                System.out.println("A new database has been created.");
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+
+}
